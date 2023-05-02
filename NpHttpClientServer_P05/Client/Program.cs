4f0046aefeef7c9d38499ba3e006ef3027fa1494
Mykyta Tishkin <87891550+mykytatishkin@users.noreply.github.com>
2023-05-02 18:34:46 +0300

    init

---
+++ NpHttpClientServer_P05/Client/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var url = "https://google.com/search?q=Hello+world";
+            //var url = "http://localhost:8080/?message=HelloServer&name=Petya&age=23";//get
+            var url = "http://localhost:8080/";//post
+            try
+            {
+                //var message = await GetRequestAsync(url);
+                var message = await PostRequestAsync(url);
+                Console.WriteLine($"Message: {message}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static async Task<string> PostRequestAsync(string url)
+        {
+            using var client = new HttpClient();
+            var values = new Dictionary<string, string>
+            {
+                {"message","Hello, Server" },
+                {"name","Vasya" },
+                {"age","23" }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            HttpResponseMessage response = await client.PostAsync(url, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Headers:");
+                foreach (var item in response.Headers)
+                {
+                    Console.Write($"{item.Key}: ");
+                    foreach (var value in item.Value)
+                    {
+                        Console.WriteLine($"{value}, ");
+                    }
+                    Console.WriteLine();
+                }
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+                throw new HttpRequestException("Something went wrong");
+            }
+
+
+        }
+        static async Task<string> GetRequestAsync(string url)
+        {
+            using var client = new HttpClient();
+
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Headers:");
+                foreach (var item in response.Headers)
+                {
+                    Console.Write($"{item.Key}: ");
+                    foreach (var value in item.Value)
+                    {
+                        Console.WriteLine($"{value}, ");
+                    }
+                    Console.WriteLine();
+                }
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+                throw new HttpRequestException("Something went wrong");
+            }
+        }
+    }
+}
---
+++ NpHttpClientServer_P05/Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Text;
+using System.Web;
+
+namespace Server
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            using var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            Console.WriteLine("Server is waiting for connections...");
+            while (true)
+            {
+                HttpListenerContext context = await listener.GetContextAsync();
+                if (context.Request.HttpMethod == "GET")
+                {
+                    await GetHandlerAsync(context);
+                } else if (context.Request.HttpMethod == "POST")
+                {
+                    await PostHandlerAsync(context);
+                }
+            }
+        }
+        static async Task PostHandlerAsync(HttpListenerContext context)
+        {
+            var message = string.Empty;
+            using (var sr = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+            {
+                message = await sr.ReadToEndAsync();
+            }
+
+            var postData = HttpUtility.ParseQueryString(message);
+            Console.WriteLine($"Message from client: {message}");
+            var name = postData["name"];
+            var age = postData["age"];
+            message = postData["message"];
+            var html = $"<html><head></head><body> Hello {name}, your age is {age} </body></html>";
+            var buf = Encoding.UTF8.GetBytes(html);
+            context.Response.ContentLength64 = buf.Length;
+            await context.Response.OutputStream.WriteAsync(buf, 0, buf.Length);
+            context.Response.OutputStream.Close();
+        }
+        static async Task GetHandlerAsync(HttpListenerContext context)
+        {
+            var message = context.Request.QueryString["message"];
+            var name = context.Request.QueryString["name"];
+            var age = context.Request.QueryString["age"];
+            Console.WriteLine($"Message from client: {message}");
+            var html = $"<html><head></head><body> Hello {name}, your age is {age} </body></html>";
+            var buf = Encoding.UTF8.GetBytes(html);
+            context.Response.ContentLength64 = buf.Length;
+            await context.Response.OutputStream.WriteAsync(buf, 0, buf.Length);
+            context.Response.OutputStream.Close();
+        }
+    }
+}
